b0b03fb64a27531ab68eb3cf9d17eeb11ef075d2
DreamMetawee <65040233119@udru.ac.th>
2025-09-27 10:57:00 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,18 +24,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-gradient-to-br from-black via-gray-900 to-gray-800 text-white min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} 
+          bg-gradient-to-br from-black via-gray-900 to-gray-800 
+          text-white min-h-screen flex`}
       >
-        <div className="bg-gradient-to-br from-black via-gray-900 to-gray-800 text-white">
-          {/* Sidebar fixed */}
+        {/* Sidebar */}
+        <aside
+          className="hidden md:flex fixed left-0 top-0 h-full w-64 
+            bg-gradient-to-b from-gray-950 via-gray-900 to-gray-800
+            border-r border-gray-700 shadow-lg"
+        >
           <Sidebar />
+        </aside>
 
-          <div className="flex-1 flex flex-col " style={{ marginLeft: "256px" }}>
-            <Header />
-            <main className="flex-1 mt-[72px] overflow-y-auto p-4 bg-gray-900 text-white">
-              {children}
-            </main>
-          </div>
+        {/* Main content */}
+        <div className="flex-1 flex flex-col ml-0 md:ml-64">
+          <Header />
+          <main className="flex-1 mt-[72px] overflow-y-auto p-4 bg-gray-900">
+            {children}
+          </main>
         </div>
       </body>
     </html>
